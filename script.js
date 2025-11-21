41ff118b756da294af82f7f983863031ab78f81e
Birhan Kassaye <birhanyalew2019@gmail.com>
2025-11-21 22:59:44 +0300

    Add the second new class in a div

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function getGrid() {
             const gridItem = document.createElement("div"); // The div created using javascript
             gridItem.textContent = `Raw${i+1} Column${j+1}`;
             gridItem.classList.add("grid-Item"); //Adding a class inside the div element
+            gridItem.classList.add("grid-Item2"); //Adding a second new class in a div
             container.append(gridItem);
         }
     }
