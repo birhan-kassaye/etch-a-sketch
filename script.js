8ea2fd66aa79620b43a1569b693d2ce405d0aaa2
Birhan Kassaye <birhanyalew2019@gmail.com>
2025-11-24 20:40:56 +0300

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const container = document.getElementById("container"); //Accessing the div container from script.js file
-
+//const container = document.getElementById("container"); //Accessing the div container from script.js file
+/*
 function getGrid() {
-    const numGrid = document.getElementById("myInput").value;
+        const numGrid = prompt("Enter the number of Grid: ");
     for(let i=0; i<numGrid; i++){
         for(j=0; j<numGrid; j++){
             const gridItem = document.createElement("div"); // The div created using javascript
@@ -12,3 +12,36 @@ function getGrid() {
         }
     }
 }
+*/
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const container = document.getElementById("container");
+    const button = document.getElementById("button");
+
+    button.addEventListener('click', ()=> {
+        container.innerHTML = '';
+        //validate and convert prompt input in to a number
+        let newGridnum = prompt("Num of Grid 1-100");
+        newGridnum = parseInt(newGridnum);
+
+        if (newGridnum >= 1 && newGridnum <= 100){
+            //Create the grid cells
+        for(let i=0; i<newGridnum; i++){
+            const divRaw = document.createElement("div");
+            divRaw.classList.add("grid-raw");
+            for(let j=0; j<newGridnum; j++){
+                const div = document.createElement("div");
+                div.textContent = `Raw${i+1} Column${j+1}`;
+                div.classList.add("grid-column");
+
+                divRaw.append(div);
+            }
+            container.append(divRaw);
+        }
+        //Removes the button after it gets clicked
+        button.remove();
+        }else{
+            alert("Please enter a number between 1-100");
+        }
+    })
+})
